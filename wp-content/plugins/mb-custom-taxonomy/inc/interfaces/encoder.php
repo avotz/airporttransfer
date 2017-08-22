52bb44bb15dd5dfcf53576b808a14df10358fb2a
Alonso Avotz <alonso@avotz.com>
2017-08-22 17:22:19 -0600

    se actualizo wp y woocommerce

--- wp-content/plugins/mb-custom-taxonomy/inc/interfaces/encoder.php
+++ wp-content/plugins/mb-custom-taxonomy/inc/interfaces/encoder.php
@@ -3,12 +3,18 @@
  * Encoder interface
  *
  * @package    Meta Box
- * @subpackage MB Custom Post Type
+ * @subpackage MB Custom Taxonomy
  */
 
 if ( ! interface_exists( 'MB_CPT_Encoder_Interface' ) ) {
 	interface MB_CPT_Encoder_Interface {
 
+		/**
+		 * Encode data.
+		 *
+		 * @param  array $data Data to encode.
+		 * @return string      Encoded string.
+		 */
 		public function encode( $data );
 	}
 }
