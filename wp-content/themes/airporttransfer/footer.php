d9e06c226172efe09005f3b2462a78cd9f0ba7bc
Alonso Avotz <alonso@avotz.com>
2018-01-29 11:47:05 -0600

    test map in tab

--- wp-content/themes/airporttransfer/footer.php
+++ wp-content/themes/airporttransfer/footer.php
@@ -96,7 +96,7 @@
 
       jQuery('body').on('click','#tab-title-map',function() {
         gmwdInitMainMap('wd-map<?php echo $shortcode_id; ?>',false, '<?php echo $shortcode_id; ?>');
-        gmwdReadyFunction(‘<?php echo $shortcode_id; ?>’);
+        gmwdReadyFunction('<?php echo $shortcode_id; ?>');
     });
      
 </script>
