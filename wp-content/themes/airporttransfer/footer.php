34909bda1ba72bd979a493fb64d6444c7cd865b2
Alonso Avotz <alonso@avotz.com>
2018-04-06 16:28:22 -0600

    update footer for form hotel

--- wp-content/themes/airporttransfer/footer.php
+++ wp-content/themes/airporttransfer/footer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying the footer
  *
@@ -16,14 +17,14 @@
         	<div class="footer-container">
 	        	<div class="copyright"><p>Guanacaste viajes © 2017</p><p class="avotz">Desarrollado por <a href="http://avotz.com" target="_blank"><i class="icon-avotz"></i></a></p>
 	                <span id="siteseal"><script async type="text/javascript" src="https://seal.godaddy.com/getSeal?sealID=SfhxEla2LlXOtSRrRy4MxCTCLdcbWU1teGmbuzj3igJ88v7tKMNen9YEmMFj"></script></span>
-	                <img src="<?php echo get_template_directory_uri();  ?>/img/cc-badges-ppppcmcvdam.png" alt="Pay with PayPal, PayPal Credit or any major credit card" class="credit-logos" />
+	                <img src="<?php echo get_template_directory_uri(); ?>/img/cc-badges-ppppcmcvdam.png" alt="Pay with PayPal, PayPal Credit or any major credit card" class="credit-logos" />
 	            </div>
 	            <div class="phone-numbers">
 	                <p><strong>USA Phone Number:</strong> +1-404-963-9097 </p><p><strong>Costa Rica:</strong> +011-506-2697-1818 | +011-506-8704-3690</p><br />
                     <a href="https://www.google.com/maps/place/Guanacaste+Viajes+%26+Tours/@10.5278787,-85.6458598,14z/data=!4m5!3m4!1s0x0:0x51628778f1f8d914!8m2!3d10.526166!4d-85.6433" target="_blank" style="color:white;">Sardinal, 75 north of Sea Wonder Academy</a><br>
 	                <!-- <div id="TA_certificateOfExcellence501" class="TA_certificateOfExcellence"><ul id="IpwE3XUB1lOP" class="TA_links jM54Pmu8W"><li id="WpGhLhOy4" class="udUMqg"><a target="_blank" href="https://www.tripadvisor.com/Attraction_Review-g309240-d2102243-Reviews-Guanacaste_Viajes_Tours-Liberia_Province_of_Guanacaste.html"><img src="https://www.tripadvisor.com/img/cdsi/img2/awards/CoE2015_WidgetAsset-14348-2.png" alt="TripAdvisor" class="widCOEImg" id="CDSWIDCOELOGO"/></a></li></ul></div><script src="https://www.jscache.com/wejs?wtype=certificateOfExcellence&uniq=501&locationId=2102243&lang=en_US&year=2015&display_version=2"></script> -->
-	                <div id="TA_certificateOfExcellence918" class="TA_certificateOfExcellence"><ul id="6KV4jNSJrj" class="TA_links B3GeyqTv1"><li id="wajqJc8nzq" class="4FY0PGBQzdf"><a target="_blank" href="https://www.tripadvisor.co.uk/Attraction_Review-g309240-d2102243-Reviews-Guanacaste_Viajes_Tours-Liberia_Province_of_Guanacaste.html"><img src="<?php echo get_template_directory_uri();  ?>/img/CoE2016_WidgetAsset-14348-2.png" alt="TripAdvisor" class="widCOEImg" id="CDSWIDCOELOGO"/></a></li></ul></div><script src="https://www.jscache.com/wejs?wtype=certificateOfExcellence&uniq=918&locationId=2102243&lang=en_UK&year=2016&display_version=2"></script>
-                    <div id="TA_certificateOfExcellence983" class="TA_certificateOfExcellence"><ul id="aRAz22zP" class="TA_links QZTzOruy7Dgo"><li id="G45DorwWaqw" class="sEXjfr4m6"><a target="_blank" href="https://www.tripadvisor.com/Attraction_Review-g309240-d2102243-Reviews-Guanacaste_Viajes_Tours-Liberia_Province_of_Guanacaste.html"><img src="<?php echo get_template_directory_uri();  ?>/img/CoE2017_WidgetAsset-14348-2.png" alt="TripAdvisor" class="widCOEImg" id="CDSWIDCOELOGO"/></a></li></ul></div><script src="https://www.jscache.com/wejs?wtype=certificateOfExcellence&uniq=983&locationId=2102243&lang=en_US&year=2017&display_version=2"></script>    
+	                <div id="TA_certificateOfExcellence918" class="TA_certificateOfExcellence"><ul id="6KV4jNSJrj" class="TA_links B3GeyqTv1"><li id="wajqJc8nzq" class="4FY0PGBQzdf"><a target="_blank" href="https://www.tripadvisor.co.uk/Attraction_Review-g309240-d2102243-Reviews-Guanacaste_Viajes_Tours-Liberia_Province_of_Guanacaste.html"><img src="<?php echo get_template_directory_uri(); ?>/img/CoE2016_WidgetAsset-14348-2.png" alt="TripAdvisor" class="widCOEImg" id="CDSWIDCOELOGO"/></a></li></ul></div><script src="https://www.jscache.com/wejs?wtype=certificateOfExcellence&uniq=918&locationId=2102243&lang=en_UK&year=2016&display_version=2"></script>
+                    <div id="TA_certificateOfExcellence983" class="TA_certificateOfExcellence"><ul id="aRAz22zP" class="TA_links QZTzOruy7Dgo"><li id="G45DorwWaqw" class="sEXjfr4m6"><a target="_blank" href="https://www.tripadvisor.com/Attraction_Review-g309240-d2102243-Reviews-Guanacaste_Viajes_Tours-Liberia_Province_of_Guanacaste.html"><img src="<?php echo get_template_directory_uri(); ?>/img/CoE2017_WidgetAsset-14348-2.png" alt="TripAdvisor" class="widCOEImg" id="CDSWIDCOELOGO"/></a></li></ul></div><script src="https://www.jscache.com/wejs?wtype=certificateOfExcellence&uniq=983&locationId=2102243&lang=en_US&year=2017&display_version=2"></script>    
 	                <!--<span id="siteseal"><script type="text/javascript" src="https://seal.starfieldtech.com/getSeal?sealID=mq6QnWtPXw00suNUVWLlYvccgxIy24zeNuwFeLjlIohOjHUEuppZUj"></script></span>
 	                </div>-->
 	                
@@ -42,17 +43,17 @@
 
      <div id="tour-popup" class="request-popup white-popup mfp-hide mfp-with-anim">
         <h2>Inquire for Tour</h2>
-         <?php  echo do_shortcode('[contact-form-7 id="90" title="Contact Tour"]') ;?>
+         <?php echo do_shortcode('[contact-form-7 id="90" title="Contact Tour"]'); ?>
               
         
     </div>
 	<div id="hotel-popup" class="request-popup white-popup mfp-hide mfp-with-anim">
-        <h2>Inquire for hotel transportation</h2>
-         <?php  echo do_shortcode('[contact-form-7 id="2141" title="Hotel Transportation form"]') ;?>
+        <h2>Inquire for hotel</h2>
+         <?php echo do_shortcode('[contact-form-7 id="90" title="Contact Tour"]'); ?>
               
         
     </div>
-   <?php /*if (is_front_page() ) : */?>
+   <?php /*if (is_front_page() ) : */ ?>
 					<!-- booking -->
                     <div class="request-popup white-popup mfp-hide mfp-with-anim" id="transfer-popup">
                         <div id='bro_reservation'></div><input type='hidden' id='cid' value='AOKdZADhufaic11ZFe5UWg%3d%3d' /><input type='hidden' id='dbaid' value='PW5zWkTv9KRUJHxtYK4OKQ%3d%3d' /> <script type='text/javascript'> (function () { var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true; po.src = 'https://bookridesonline.com/web/reservation.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s); })(); </script>
@@ -63,7 +64,7 @@
                         (function () { var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true; po.src = 'https://bookridesonline.com/web/reservation.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s); })(); 
                         </script> -->
                     </div>
-	<?php/*
+	<? php/*
         endif;*/ ?>
     
 <?php wp_footer(); ?>
