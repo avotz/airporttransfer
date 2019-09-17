04a1a72f1de2c265cf1e9220f075451d4b60b0a5
Alonso Chavarria <alonso@avotz.com>
2019-09-17 12:40:15 -0600

    se quito comentario de jquery por q no se utliza

--- wp-content/themes/guanacasteviajes/resources/js/config.js
+++ wp-content/themes/guanacasteviajes/resources/js/config.js
@@ -5,12 +5,6 @@ import AOS from 'aos';
 window.AOS = AOS;
 import 'aos/dist/aos.css';
 
-try {
-    //window.$ = window.jQuery = require('jquery');
-
-} catch (e) { }
-
-
 window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
--- wp-content/themes/guanacasteviajes/webpack.mix.js
+++ wp-content/themes/guanacasteviajes/webpack.mix.js
@@ -2,6 +2,9 @@ const mix = require('laravel-mix');
 let tailwindcss = require('tailwindcss');
 
 mix.js('resources/js/app.js', 'js')
+    // .autoload({
+    //     jquery: ['$', 'window.jQuery', 'jQuery']
+    // })
     .sass('resources/sass/style.scss', './')
     .options({
         processCssUrls: false,
