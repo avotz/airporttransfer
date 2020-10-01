5f1bfeff3ff48b55d6dd71619bfa25f1dd9ab6e6
Alonso Chavarria <alonso@avotz.com>
2020-10-01 15:43:42 -0600

    test kkstar in hotels

--- wp-content/themes/guanacasteviajes/page-book-hotels.php
+++ wp-content/themes/guanacasteviajes/page-book-hotels.php
@@ -90,7 +90,7 @@ get_header(); ?>
 										</h4>
 									</div>
 									<div class="rating">
-										<?php if (function_exists("kk_star_ratings")) : echo kk_star_ratings($pid);
+										<?php if (function_exists("kk_star_ratings")) : echo kk_star_ratings();
 										endif; ?>
 									</div>
 									<div class="price">
