95933d90d922065885d6a72cc5f68ac99bc3e66b
Alonso Chavarria <alonso@avotz.com>
2021-04-30 11:37:51 -0600

    se agrego wc

--- wp-content/themes/guanacasteviajes/inc/wc.php
+++ wp-content/themes/guanacasteviajes/inc/wc.php
@@ -28,11 +28,11 @@ function st_woocommerce_shop_url()
   return site_url();
 }
 /** woocommerce **/
-//add_filter('woocommerce_output_related_products_args', 'jk_related_products_args');
+add_filter('woocommerce_output_related_products_args', 'jk_related_products_args');
 function jk_related_products_args($args)
 {
-  $args['posts_per_page'] = 4; // 4 related products
-  $args['columns'] = 4; // arranged in 2 columns
+  $args['posts_per_page'] = 3; // 4 related products
+  $args['columns'] = 3; // arranged in 2 columns
   return $args;
 }
 
